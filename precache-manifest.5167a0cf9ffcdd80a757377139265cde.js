bf2cf4235a4908ac0293a1f8ac65c669989f553e
Benyomin Walters <chassidus@gmail.com>
2019-06-26 11:46:22 -0500

    Updates

---
+++ precache-manifest.5167a0cf9ffcdd80a757377139265cde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17180e7427387b35e2c94e03a0d746a6",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "59522a08c6b1d3178e8a",
+    "url": "/Robofriends/static/css/2.3cf8dc49.chunk.css"
+  },
+  {
+    "revision": "b815b64cb9164b4d1354",
+    "url": "/Robofriends/static/css/main.53b7822e.chunk.css"
+  },
+  {
+    "revision": "59522a08c6b1d3178e8a",
+    "url": "/Robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "b815b64cb9164b4d1354",
+    "url": "/Robofriends/static/js/main.22157e87.chunk.js"
+  },
+  {
+    "revision": "47afcf7c1fdce8fc928a",
+    "url": "/Robofriends/static/js/runtime~main.4fea8ee0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
